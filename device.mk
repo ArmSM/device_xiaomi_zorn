92abd4a8b81902e9377f138185110142790b1de3
momenabdulrazek <momenabdulrazek@users.noreply.github.com>
2026-03-26 22:44:29 +0100

    Overlay: rename CarrierConfig to avoid conflict with source one

--- device.mk
+++ device.mk
@@ -333,7 +333,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
-    CarrierConfig \
+    CarrierConfigZorn \
     FrameworksResXiaomi \
     FrameworksResZorn \
     SystemUIResZorn \
